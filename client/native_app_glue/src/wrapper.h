edfe2f70f88633e619e9e7dd34324c89a0d48fee
lzebulon <lzebulon@outlook.fr>
2023-11-17 23:04:04 +0100

    fix link to native_app_glue

--- client/native_app_glue/src/wrapper.h
+++ client/native_app_glue/src/wrapper.h
@@ -1,6 +1,6 @@
 #ifndef WRAPPER_H_
 #define WRAPPER_H_
 
-#include "../../android/ndk/sources/android/native_app_glue_modified/android_native_app_glue.h"
+#include "../../android/ndk/sources/android/native_app_glue/android_native_app_glue.h"
 
 #endif /* WRAPPER_H_ */
